8b1bc1b101669340e7025e7e3769e8ad83f6c0f1
Pieter Pel <pelpieter@gmail.com>
2025-08-12 09:54:21 +0200

    Added prometheus target setup back

--- modules/prometheus.nix
+++ modules/prometheus.nix
@@ -1,12 +1,12 @@
 { config, ... }:
 let
   clientPort = 3021;
-  #
-  # remoteTargets = builtins.attrValues (
-  #   builtins.mapAttrs (_: host: "${host.ip}:${toString clientPort}") config.hosts
-  # );
-  #
-  targets = [ "127.0.0.1:${toString clientPort}" ]; # ++ remoteTargets;
+
+  remoteTargets = builtins.attrValues (
+    builtins.mapAttrs (_: host: "${host.ip}:${toString clientPort}") config.hosts
+  );
+
+  targets = [ "127.0.0.1:${toString clientPort}" ] ++ remoteTargets;
 in
 {
   networking.firewall.allowedTCPPorts = [ clientPort ];
