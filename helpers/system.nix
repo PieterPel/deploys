6269dd2469c2e54d3fdbd345bfc3a52475ae2c7c
Pieter Pel <pelpieter@gmail.com>
2025-08-12 09:54:36 +0200

    Use binary cache from nixpkgs for deploy-rs using overlay

--- helpers/system.nix
+++ helpers/system.nix
@@ -5,28 +5,36 @@
 ,
 }:
 let
+  # NOTE:: deploy-rs now uses binary cache of nixpkgs
+  pkgs = import inputs.nixpkgs { inherit system; };
+  deployPkgs = import inputs.nixpkgs {
+    inherit system;
+    overlays = [
+      inputs.deploy-rs.overlays.default
+      (self: super: {
+        deploy-rs = {
+          inherit (pkgs) deploy-rs;
+          lib = super.deploy-rs.lib;
+        };
+      })
+    ];
+  };
 
+  # Helper function to extend a host form my dotfiles
   mkNixOS =
     modules:
     inputs.dotfiles.nixosConfigurations.${hostname}.extendModules {
       inherit modules;
     };
 
-  # mkNode = ipaddress: {
-  #   hostname = ipaddress;
-  #   profiles.system = {
-  #     user = "root";
-  #     sshUser = "deploy";
-  #     path = inputs.deploy-rs.lib.${system}.activate.nixos self.nixosConfigurations.${hostname};
-  #   };
-  # };
-  #
+  # Default system profile for deployment
   systemProfile = {
     user = "root";
     sshUser = "deploy";
-    path = inputs.deploy-rs.lib.${system}.activate.nixos self.nixosConfigurations.${hostname};
+    path = deployPkgs.deploy-rs.lib.activate.nixos self.nixosConfigurations.${hostname};
   };
 
+  # Helper function to generate an app profile for deploy-rs
   mkAppProfile =
     { user
     , package
@@ -38,11 +46,12 @@ let
     {
       inherit user;
       inherit sshUser;
-      path = inputs.deploy-rs.lib.${system}.activate.custom package script;
+      path = deployPkgs.deploy-rs.lib.activate.custom package script;
       remoteBuild = system != "x86_64-linux";
     }
     // (if profilePath != null then { inherit profilePath; } else { });
 
+  # Helper function to generate a node for deploy-rs
   mkNode =
     { hostname
     , profiles
